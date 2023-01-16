bade7bb7c1a3c0fdc873e847c632e9d07e4605f3
Edwin Rafael Larrea Buste <edwin.larreab@ug.edu.ec>
2023-01-16 15:57:02 -0500

    percepcion
    impl firebase database

--- app/src/main/java/com/dennisgutierrez/dam_g7_proyecto_1p/PercepcionActivity.java
+++ app/src/main/java/com/dennisgutierrez/dam_g7_proyecto_1p/PercepcionActivity.java
@@ -1,5 +1,7 @@
 package com.dennisgutierrez.dam_g7_proyecto_1p;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,30 +9,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class PercepcionActivity extends AppCompatActivity {
-    private FirebaseFirestore firestoreService;
+    private FirebaseFirestore bdService;
+    private FirebaseStorage stService  ;
     private String IdUsuario;
 
 
     //region variables para Timer
     private Chronometer Cronometro;
     private long timeLeft_ms=0;
+    //endregion
+
+    //region variables para preguntas
+    private String repuesta;
+    private int intentos;
     //endregion
 
     //region Inicializacion de Objetos
@@ -45,7 +59,8 @@ public class PercepcionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_percepcion);
 
         //Instancia la base de Firestore
-        firestoreService= FirebaseFirestore.getInstance();
+        bdService= FirebaseFirestore.getInstance();
+        stService = FirebaseStorage.getInstance();
 
         //Configuracion de la vista
         this.setTitle("Percepción");
@@ -59,6 +74,8 @@ public class PercepcionActivity extends AppCompatActivity {
         //Instancia TimerLapse
         Cronometro= (Chronometer) findViewById(R.id.Cronometro);
         TimerStart();
+
+        CargarPreguntas();
 
         //region Intancia de Objetos
         txtScore= (TextView) findViewById(R.id.txtScore);
@@ -95,13 +112,48 @@ public class PercepcionActivity extends AppCompatActivity {
     }
     //endregion
 
+    //region Preguntas
+    private void  CargarPreguntas()
+    {
+        intentos=0;
+        repuesta="";
+
+        FirebaseFirestore bd= FirebaseFirestore.getInstance();
+        DocumentReference pregunta= bd.collection("percepcion").document("Q001");
+
+        pregunta.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Toast.makeText(PercepcionActivity.this, document.get("ImgBase").toString(), Toast.LENGTH_LONG).show();
+
+
+                        txtIndicaciones.setText(document.get("indicaciones").toString());
+                        imgReal.setImageResource(percepcion_library.getImagen(document.get("ImgBase").toString()));
+
+                    }
+                    else{
+                        Toast.makeText(PercepcionActivity.this,"No such document", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    txtIndicaciones.setText("get failed with " + task.getException().toString());
+                    Toast.makeText(PercepcionActivity.this, "get failed with " + task.getException(), Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+
+    //endregion
 
     //código para guardar en firebase
     private void postSave(String value){
         Map<String, Object> map= new HashMap<>();
         map.put("clave",value);
 
-        firestoreService.collection("nombrecoleecion").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        bdService.collection("nombrecoleecion").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
             public void onSuccess(DocumentReference documentReference) {
 
---
+++ app/src/main/java/com/dennisgutierrez/dam_g7_proyecto_1p/percepcion_library.java
@@ -0,0 +1,105 @@
+package com.dennisgutierrez.dam_g7_proyecto_1p;
+
+import androidx.annotation.NonNull;
+
+public class percepcion_library {
+
+    public static  int getImagen(@NonNull String imgNombre)
+    {
+        switch (imgNombre.trim().toUpperCase())
+        {
+            case "M01":
+                return  R.drawable.ic_m01;
+            case "M02":
+                return  R.drawable.ic_m02;
+            case "M03":
+                return  R.drawable.ic_m03;
+            case "M04":
+                return  R.drawable.ic_m04;
+            case "M05":
+                return  R.drawable.ic_m05;
+            case "M06":
+                return  R.drawable.ic_m07;
+            case "M08":
+                return  R.drawable.ic_m08;
+            case "M09":
+                return  R.drawable.ic_m09;
+            case "M10":
+                return  R.drawable.ic_m10;
+            case "M11":
+                return  R.drawable.ic_m11;
+            case "M12":
+                return  R.drawable.ic_m12;
+            case "M13":
+                return  R.drawable.ic_m13;
+            case "M14":
+                return  R.drawable.ic_m14;
+            case "M15":
+                return  R.drawable.ic_m15;
+            case "M16":
+                return  R.drawable.ic_m16;
+            case "M17":
+                return  R.drawable.ic_m17;
+            case "M18":
+                return  R.drawable.ic_m18;
+            case "M19":
+                return  R.drawable.ic_m19;
+            case "M20":
+                return  R.drawable.ic_m20;
+            case "M21":
+                return  R.drawable.ic_m21;
+            case "M22":
+                return  R.drawable.ic_m22;
+            case "M23":
+                return  R.drawable.ic_m23;
+            case "M24":
+                return  R.drawable.ic_m24;
+            case "M25":
+                return  R.drawable.ic_m25;
+            case "M26":
+                return  R.drawable.ic_m26;
+            case "M27":
+                return R.drawable.ic_m27;
+            case "M28":
+                return  R.drawable.ic_m28;
+            case "M29":
+                return  R.drawable.ic_m29;
+            case "M30":
+                return  R.drawable.ic_m30;
+            case "M31":
+                return  R.drawable.ic_m31;
+            case "M32":
+                return  R.drawable.ic_m32;
+            case "M33":
+                return  R.drawable.ic_m33;
+            case "M34":
+                return  R.drawable.ic_m34;
+            case "M35":
+                return  R.drawable.ic_m35;
+            case "M36":
+                return  R.drawable.ic_m36;
+            case "M37":
+                return  R.drawable.ic_m37;
+            case "M38":
+                return  R.drawable.ic_m38;
+
+            default:
+            return R.drawable.ic_uglogo;
+        }
+    }
+
+    public static  int getIndicaciones(@NonNull String strNombre)
+    {
+        switch (strNombre.trim().toUpperCase()) {
+            case "IGUAL":
+                return R.string.percep_strindica1;
+            case "MENOR":
+                return R.string.percep_strindica2;
+            case "MAYOR":
+                return R.string.percep_strindica3;
+            default:
+                return  R.string.percep_strindica_error;
+        }
+
+    }
+}
