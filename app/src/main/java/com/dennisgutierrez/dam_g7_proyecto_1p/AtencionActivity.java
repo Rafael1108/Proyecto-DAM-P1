44181358d7d9d82f92cb0d15beb4d1dd5ddd816b
Iván Espinoza de los Monteros <ivanespinoza2499@gmail.com>
2023-01-18 04:48:22 -0500

    Firebase y funcionalidad en módulo atención, falta por agregar más minijuegos

--- app/src/main/java/com/dennisgutierrez/dam_g7_proyecto_1p/AtencionActivity.java
+++ app/src/main/java/com/dennisgutierrez/dam_g7_proyecto_1p/AtencionActivity.java
@@ -1,11 +1,16 @@
 package com.dennisgutierrez.dam_g7_proyecto_1p;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -19,18 +24,26 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class AtencionActivity extends AppCompatActivity {
 
+    private static int MAXPREGUNTAS=3;
     private FirebaseFirestore bdService;
-    private FirebaseStorage stService  ;
+    //private FirebaseStorage stService  ;
     private String IdUsuario;
 
 
@@ -40,14 +53,22 @@ public class AtencionActivity extends AppCompatActivity {
     //endregion
 
     //region variables para preguntas
-    private String repuesta;
+    private String respuesta;
+    private String idJuego;
     private int intentos;
+    //endregion
+
+
+    //region variables para score
+    private int scoreActual;
+    private int totalJuegos;
+    private int totalIntentos;
     //endregion
 
     //region Inicializacion de Objetos
     TextView txtScore,txtIndicaciones;
     ImageView imgBase,imgAt1,imgAt2,imgAt3,imgAt4,imgAt5,imgAt6,imgAt7,imgAt8,imgAt9,
-    imgAt10,imgAt11,imgAt12,imgAt13,imgAt14,imgAt15;
+    imgAt10,imgAt11,imgAt12,imgAt13,imgAt14,imgAt15,imgAt16,imgAt17,imgAt18,imgAt19,imgAt20;
 
     Button atencionRegresarButton;
 
@@ -58,7 +79,7 @@ public class AtencionActivity extends AppCompatActivity {
 
         //Instancia la base de Firestore
         bdService= FirebaseFirestore.getInstance();
-        stService = FirebaseStorage.getInstance();
+        //stService = FirebaseStorage.getInstance();
 
         //Configuracion de la vista
         this.setTitle("Atención");
@@ -71,9 +92,9 @@ public class AtencionActivity extends AppCompatActivity {
 
         //Instancia TimerLapse
         Cronometro= (Chronometer) findViewById(R.id.Cronometro);
-        TimerStart();
+        //TimerStart();
 
-        CargarPreguntas();
+        //CargarPreguntas();
 
         //region Intancia de Objetos
         txtScore= (TextView) findViewById(R.id.txtScore);
@@ -94,6 +115,12 @@ public class AtencionActivity extends AppCompatActivity {
         imgAt13= (ImageView) findViewById(R.id.imgAt13);
         imgAt14= (ImageView) findViewById(R.id.imgAt14);
         imgAt15= (ImageView) findViewById(R.id.imgAt15);
+        imgAt16= (ImageView) findViewById(R.id.imgAt16);
+        imgAt17= (ImageView) findViewById(R.id.imgAt17);
+        imgAt18= (ImageView) findViewById(R.id.imgAt18);
+        imgAt19= (ImageView) findViewById(R.id.imgAt19);
+        imgAt20= (ImageView) findViewById(R.id.imgAt20);
+
 
         atencionRegresarButton = findViewById(R.id.atencionRegresarButton);
 
@@ -105,6 +132,9 @@ public class AtencionActivity extends AppCompatActivity {
                 finish();
             }
         });
+        cargarScoreUsuario();
+        buscarPregunta();
+        txtIndicaciones.setText(R.string. percep_strindica_found);
 
     }
 
@@ -118,14 +148,91 @@ public class AtencionActivity extends AppCompatActivity {
     }
     //endregion
 
-    //region Preguntas
-    private void  CargarPreguntas()
-    {
-        intentos=0;
-        repuesta="";
 
-        FirebaseFirestore bd= FirebaseFirestore.getInstance();
-        DocumentReference pregunta = bd.collection("atencion").document("Q002");
+    //region Preguntas
+    private void limpiarPreguntas()
+    {
+        //progStatus.setVisibility(View.VISIBLE);
+        intentos=1;
+        respuesta = "";
+        txtIndicaciones.setText(R.string. percep_strindica_found);
+        imgBase.setImageResource(0);
+        imgAt1.setImageResource(0);
+        imgAt2.setImageResource(0);
+        imgAt3.setImageResource(0);
+        imgAt4.setImageResource(0);
+        imgAt5.setImageResource(0);
+        imgAt6.setImageResource(0);
+        imgAt7.setImageResource(0);
+        imgAt8.setImageResource(0);
+        imgAt9.setImageResource(0);
+        imgAt10.setImageResource(0);
+        imgAt11.setImageResource(0);
+        imgAt12.setImageResource(0);
+        imgAt13.setImageResource(0);
+        imgAt14.setImageResource(0);
+        imgAt15.setImageResource(0);
+        imgAt16.setImageResource(0);
+        imgAt17.setImageResource(0);
+        imgAt18.setImageResource(0);
+        imgAt19.setImageResource(0);
+        imgAt20.setImageResource(0);
+    }
+
+    private void buscarPregunta()
+    {
+        if (totalJuegos>MAXPREGUNTAS)
+        {
+            limpiarPreguntas();
+            txtIndicaciones.setText(R.string.percep_strindica_error);
+        }
+        else {
+            //Se busca un juego al azar
+            Random rand = new Random();
+            int intId = rand.nextInt(MAXPREGUNTAS);
+            //para no empezar en 0
+            intId++;
+            String tmpPregunta = "Q" + intId;
+
+
+            //Crga en memoria la coleccion
+            CollectionReference logUsuario = bdService.collection("atencion_score").document(IdUsuario).collection("logs");
+
+            // Consulta, se verifica si el juego ya fue realizado.
+            Query query = logUsuario.whereEqualTo("idjuego", tmpPregunta);
+
+            query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        int rows = 0;
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            rows++;
+                        }
+
+                        if (rows == 0) {
+                            CargarPreguntas(tmpPregunta);
+                        } else {
+                            buscarPregunta();
+                        }
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                }
+            });
+        }
+    }
+
+
+
+    private void  CargarPreguntas(String juego)
+    {
+        //intentos=0;
+        //repuesta="";
+        //FirebaseFirestore bd= FirebaseFirestore.getInstance();
+        limpiarPreguntas();
+        idJuego=juego;
+        DocumentReference pregunta = bdService.collection("atencion").document(juego);
 
         pregunta.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -135,14 +242,31 @@ public class AtencionActivity extends AppCompatActivity {
                     if (document.exists()) {
                 //        Toast.makeText(AtencionActivity.this, document.get("ImgBase").toString(), Toast.LENGTH_LONG).show();
 
-
                         txtIndicaciones.setText(atencion_library.getIndicaciones(document.get("indicaciones").toString()));
+                        respuesta= document.get("respuesta").toString().toUpperCase();
                         imgBase.setImageResource(atencion_library.getImagen(document.get("ImgBase").toString()));
                         imgAt1.setImageResource(atencion_library.getImagen(document.get("ImgAt1").toString()));
                         imgAt2.setImageResource(atencion_library.getImagen(document.get("ImgAt2").toString()));
-                        imgAt3.setImageResource(atencion_library.getImagen(document.get("ImgAt1").toString()));
-                        imgAt4.setImageResource(atencion_library.getImagen(document.get("ImgAt2").toString()));
-                        imgAt5.setImageResource(atencion_library.getImagen(document.get("ImgAt1").toString()));
+                        imgAt3.setImageResource(atencion_library.getImagen(document.get("ImgAt3").toString()));
+                        imgAt4.setImageResource(atencion_library.getImagen(document.get("ImgAt4").toString()));
+                        imgAt5.setImageResource(atencion_library.getImagen(document.get("ImgAt5").toString()));
+                        imgAt6.setImageResource(atencion_library.getImagen(document.get("ImgAt6").toString()));
+                        imgAt7.setImageResource(atencion_library.getImagen(document.get("ImgAt7").toString()));
+                        imgAt8.setImageResource(atencion_library.getImagen(document.get("ImgAt8").toString()));
+                        imgAt9.setImageResource(atencion_library.getImagen(document.get("ImgAt9").toString()));
+                        imgAt10.setImageResource(atencion_library.getImagen(document.get("ImgAt10").toString()));
+                        imgAt11.setImageResource(atencion_library.getImagen(document.get("ImgAt11").toString()));
+                        imgAt12.setImageResource(atencion_library.getImagen(document.get("ImgAt12").toString()));
+                        imgAt13.setImageResource(atencion_library.getImagen(document.get("ImgAt13").toString()));
+                        imgAt14.setImageResource(atencion_library.getImagen(document.get("ImgAt14").toString()));
+                        imgAt15.setImageResource(atencion_library.getImagen(document.get("ImgAt15").toString()));
+                        imgAt16.setImageResource(atencion_library.getImagen(document.get("ImgAt16").toString()));
+                        imgAt17.setImageResource(atencion_library.getImagen(document.get("ImgAt17").toString()));
+                        imgAt18.setImageResource(atencion_library.getImagen(document.get("ImgAt18").toString()));
+                        imgAt19.setImageResource(atencion_library.getImagen(document.get("ImgAt19").toString()));
+                        imgAt20.setImageResource(atencion_library.getImagen(document.get("ImgAt20").toString()));
+
+                        TimerStart();
 
                     }
                     else{
@@ -159,22 +283,98 @@ public class AtencionActivity extends AppCompatActivity {
 
     //endregion
 
-    //código para guardar en firebase
-    private void postSave(String value){
-        Map<String, Object> map= new HashMap<>();
-        map.put("clave",value);
+    public void onImgClick(View v){
+        ImageView ImgOpc= (ImageView) v;
+        if(v.getTransitionName().trim().toUpperCase().equals(respuesta)){
+            Cronometro.stop();
+            guardarProgreso();
+            actualizarScoreUsuario();
+            Toast.makeText(AtencionActivity.this, "Correcto!", Toast.LENGTH_LONG).show();
+            buscarPregunta();
+        }
+        else {
+            Toast.makeText(AtencionActivity.this, "Intento # "+intentos + " incorrecto!", Toast.LENGTH_SHORT).show();
+            intentos ++;
+        }
+    }
 
-        bdService.collection("nombrecoleccion").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+    //region Métodos CRUD en firebase
+    private void guardarProgreso(){
+        Map<String, Object> progreso= new HashMap<>();
+        progreso.put("intentos",intentos);
+        progreso.put("tiempo",Cronometro.getText());
+        progreso.put("idjuego",idJuego);
+
+
+        //almacena el log progreso
+        bdService.collection("atencion_score")
+                .document(IdUsuario).collection("logs").document(Calendar.getInstance().getTime().toString()).set(progreso)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+    private void cargarScoreUsuario(){
+        DocumentReference pregunta= bdService.collection("atencion_score").document(IdUsuario);
+        pregunta.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
-            public void onSuccess(DocumentReference documentReference) {
+            public void onEvent(@Nullable DocumentSnapshot document,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
 
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
+                if (document != null && document.exists()) {
+                    totalIntentos= Integer.parseInt( document.get("intentos").toString() );
+                    totalJuegos= Integer.parseInt( document.get("total_juegos").toString() );
+                    scoreActual= Integer.parseInt( document.get("tasa_exito").toString() );
 
+                } else {
+                    totalIntentos= 0;
+                    totalJuegos=0;
+                    scoreActual= 100;
+                }
+
+                //progAciertos.setProgress(scoreActual);
+                txtScore.setText(scoreActual + "% de aciertos." );
             }
         });
+
     }
+    private void actualizarScoreUsuario(){
+        totalJuegos++;
+        totalIntentos+= intentos;
+        Map<String, Object> score= new HashMap<>();
+        score.put("intentos", (totalIntentos));
+        score.put("total_juegos", totalJuegos );
+        score.put("tasa_exito",Integer.divideUnsigned(totalJuegos*100,totalIntentos) );
+
+
+        bdService.collection("atencion_score")
+                .document(IdUsuario)
+                .set(score)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+    //endregion
 
 }
--- app/src/main/java/com/dennisgutierrez/dam_g7_proyecto_1p/atencion_library.java
+++ app/src/main/java/com/dennisgutierrez/dam_g7_proyecto_1p/atencion_library.java
@@ -7,6 +7,15 @@ public class atencion_library {
     {
         switch (imgNombre.trim().toUpperCase())
         {
+
+            case "M35":
+                return  R.drawable.ic_m35;
+            case "M36":
+                return  R.drawable.ic_m36;
+            case "M37":
+                return  R.drawable.ic_m37;
+            case "M38":
+                return  R.drawable.ic_m38;
             case "M39":
                 return  R.drawable.ic_m39;
             case "M40":
@@ -15,6 +24,32 @@ public class atencion_library {
                 return  R.drawable.ic_m41;
             case "M42":
                 return  R.drawable.ic_m42;
+            case "M43":
+                return  R.drawable.ic_m43;
+            case "M44":
+                return  R.drawable.ic_m44;
+            case "M45":
+                return  R.drawable.ic_m45;
+            case "M46":
+                return  R.drawable.ic_m46;
+            case "M47":
+                return  R.drawable.ic_m47;
+            case "M48":
+                return  R.drawable.ic_m48;
+            case "M49":
+                return  R.drawable.ic_m49;
+            case "M50":
+                return  R.drawable.ic_m50;
+            case "M51":
+                return  R.drawable.ic_m51;
+            case "M52":
+                return  R.drawable.ic_m52;
+            case "M53":
+                return  R.drawable.ic_m53;
+            case "M54":
+                return  R.drawable.ic_m54;
+            case "M55":
+                return  R.drawable.ic_m55;
             default:
                 return R.drawable.ic_uglogo;
         }
